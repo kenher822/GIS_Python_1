e8749ab9957f2ee43d6ec03d7355410b2c530f1d
kherrera <kenher822@gmail.com>
2019-10-24 22:53:59 -0600

    Agregando archivo 28 de script

--- Nivel_01/Tema_01/28_Describe_una_FC.py
+++ Nivel_01/Tema_01/28_Describe_una_FC.py
@@ -5,4 +5,13 @@ import arcpy
 arcpy.env.workspace = r"D:\Este Equipo\OneDrive\Cursos\GIS-Python\Nivel_01\Tema_09\Tema_09.gdb\CentralesHidroelectricas"
 
 # Obtener las caracteristicas del FC
+descFC = arcpy.Describe(arcpy.env.workspace)
 
+# Mostar el nombre, la ruta, tipo de geometría y el tipo de dataset
+print "Caracteristicas del FC:\n-----------------------------------------------------------------------------------"
+
+print " Nombre\t\t: {0}\n Ruta\t: {1}\n Geometria\t\t: {2}\n Dataset\t\t: {3}\n Datatype\t\t: {4} ".format(descFC.name,
+                                                                                                       descFC.path,
+                                                                                                       descFC.shapeType,
+                                                                                                       descFC.datasetType,
+                                                                                                       descFC.dataType)
