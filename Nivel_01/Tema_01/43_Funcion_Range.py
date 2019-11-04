295f1b4f0440f37eb85dac5f230861f7c36952bc
kherrera <kenher822@gmail.com>
2019-11-04 11:14:52 -0600

    Nivel_01/Tema_01/43_Funcion_Range.py

--- Nivel_01/Tema_01/43_Funcion_Range.py
+++ Nivel_01/Tema_01/43_Funcion_Range.py
@@ -39,3 +39,33 @@ rango = int(max(potencia)-min(potencia))
 # Calcular el numero de intervalos, aplicando la regla de Sturges (k = 1+3.3*log10(n)) # de registros
 numeroIntervalos = int(round(1+3.2*math.log10(len(potencia))))
 print "El numero de intervalos es: ", numeroIntervalos
+
+# Calcular la amplitud (amplitud = rango/numeroIntervalos)
+amplitud = rango/numeroIntervalos
+print "La amplitud es: ", amplitud
+
+# Paso 06: Calcular los valores de cada intervalo
+
+# Calcular los primeros valores de cada intervalo (Funcion Range)
+intervalo1 = range(int(min(potencia)), int(max(potencia)), amplitud)
+print "Los primeros valores de los intervalos son: ", intervalo1
+
+# Paso 07: Calcular los intervalos
+
+# Crear una lista de intervalos (vacia)
+intervalos = []
+
+# Calcular los segundos valores y los intervalos
+for i in intervalo1:
+    intervalo2 = "["  + str(i) + ";" + str(i+amplitud) + ">"
+    intervalos.append(intervalo2)
+
+print "Los intervalos son: ", intervalos
+
+# Paso 08: Crear el campo intervalos
+
+# Crear el campo Intervalos
+arcpy.AddField_management(fc, campoIntervalos, "TEXT","","","25")
+print "Campo creado"
+
+
