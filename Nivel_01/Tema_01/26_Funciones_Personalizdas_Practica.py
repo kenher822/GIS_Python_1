2aa88c6956d342a8c2e65468d56b8f8d6bde5fee
kherrera <kenher822@gmail.com>
2019-11-04 18:31:40 -0600

    Modificacion en dirección de variables de entorno de la funciones personalizadas

--- Nivel_01/Tema_01/26_Funciones_Personalizdas_Practica.py
+++ Nivel_01/Tema_01/26_Funciones_Personalizdas_Practica.py
@@ -1,4 +1,4 @@
-# Paso 01: Declarar Librerías y Variables
+# Paso 01: Declarar Librerias y Variables
 
 # Importar la libreria del ArcPy
 import arcpy
@@ -6,8 +6,8 @@ import arcpy
 # Sobreescribir los archivos de salidas
 arcpy.env.overwriteOutput = True
 
-# Especificar el FeatureClass a trabaar "Centrales Hidroelectricas"
-entidad = r"D:\Este Equipo\OneDrive\Cursos\GIS-Python\Nivel_01\Tema_08\Tema_08.gdb\CentralesHidroelectricas"
+# Especificar el FeatureClass a trabaar "Centrales Hidroelectricas" D:\Proyectos\GIS_Python_1\Nivel_01\Tema_08\Tema_08.gdb
+entidad = r"D:\Proyectos\GIS_Python_1\Nivel_01\Tema_08\Tema_08.gdb\CentralesHidroelectricas"
 
 # Recuperar todos los valores del FeatureClass
 registros = arcpy.SearchCursor(entidad)
@@ -29,9 +29,9 @@ print valores
 
 # ###########################################################################################################
 
-# Paso 03: Obtener la media aritmética de los valores obtenidos
+# Paso 03: Obtener la media aritmatica de los valores obtenidos
 
-# Construir un función para obetener la media aritmética
+# Construir un funcion para obetener la media aritmetica
 def media(lista):
     total = 0
     for i in lista:
@@ -39,32 +39,32 @@ def media(lista):
     media = total/len(lista)
     return media
 
-# Llamar a la función media
+# Llamar a la funcion media
 resultadoMedia = media(valores)
 
 print resultadoMedia
 
 # ###########################################################################################################
 
-# Paso 04: Seleccionar los valores mayores a la media aritmética
+# Paso 04: Seleccionar los valores mayores a la media aritmetica
 
-# Crear una capa temporal que va a ser copia del FeatureClass CentralesHidroelécticas
+# Crear una capa temporal que va a ser copia del FeatureClass CentralesHidroelecticas
 arcpy.MakeFeatureLayer_management(entidad, "entidad_temporal")
 
-# Crear una expresión que me indique: mayor a la media(Potencia>84.53333)
+# Crear una expresion que me indique: mayor a la media(Potencia>84.53333)
 expresion = "Potencia>"+str(resultadoMedia)
 
-# Seleccionar los registros mayores a la media aritmética
+# Seleccionar los registros mayores a la media aritmetica
 arcpy.SelectLayerByAttribute_management("entidad_temporal","NEW_SELECTION",expresion)
 
 # ###########################################################################################################
 
-# Paso 05: Pasar la selección de la capa temporal a una capa fisica
+# Paso 05: Pasar la seleccion de la capa temporal a una capa fisica
 
-# Especificar la ruta de la capa física
-entidadMayorMedia = "D:\Este Equipo\OneDrive\Cursos\GIS-Python\Nivel_01\Tema_08\Tema_08.gdb\CentralesHidroelectricasMayorMedia"
+# Especificar la ruta de la capa fisica
+entidadMayorMedia = r"D:\Proyectos\GIS_Python_1\Nivel_01\Tema_08\Tema_08.gdb\CentralesHidroelectricasMayorMedia"
 
-# Pasar los valores seleccionados a la capa física
+# Pasar los valores seleccionados a la capa fisica
 arcpy.Select_analysis("entidad_temporal",entidadMayorMedia)
 
 # Eliminar la capa temporal
