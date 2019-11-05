658d47514e0ac5997fba3254891b1f489caf596d
kherrera <kenher822@gmail.com>
2019-11-04 18:35:12 -0600

    Agregando add in geoprocesamiento

---
+++ Nivel_01/Tema_16/Herramienta/Install/Herramienta_addin.py
@@ -0,0 +1,42 @@
+import arcpy
+import pythonaddins
+
+class ButtonClass1(object):
+    """Implementation for Herramienta_addin.button_Ayuda (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        pythonaddins.MessageBox("Herramienta para geoprocesamiento SISCAT","SISCAT PY",0)
+
+class ButtonClass15(object):
+    """Implementation for Herramienta_addin.button_Desactivar_Capas (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        pass
+
+class ButtonClass16(object):
+    """Implementation for Herramienta_addin.button_GenerarID (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        pass
+
+class ButtonClass7(object):
+    """Implementation for Herramienta_addin.button_Buffer (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        pass
+
+class ButtonClass8(object):
+    """Implementation for Herramienta_addin.button_Activar_Capas (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        pass
